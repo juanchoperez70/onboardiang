e187a628d6d743480410b424de0b7f15ce2a6938
Juan Perez <jperez@altavistaed.com>
2020-04-15 14:16:46 +0000

    PR comments fixes

--- onboarding/app/controllers/customer_controller.rb
+++ onboarding/app/controllers/customer_controller.rb
@@ -1,29 +1,34 @@
 class CustomerController < ApplicationController
-  before_action :require_active_customer
+  before_action :require_active_customer, only:[:find_all]
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
 
   def create
-    @customer = Customer.new(params[:customer])
+R   @customer = Customer.new(customer_params)
     @customer.save
     redirect_to @customer
   end
 
   def find_all
-    @customers = Customer.all
+  R  @customers = Customer.all
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @customers }
     end
   end
 
-  private
-
-  def active_customer
+  private def active_customer
     #check if customer is activo
     @customer = Customer.find(params[:id])
 	  unless  @customer.active?
       flash[:error] = "Customer must be active"
+      redirect_to @customer
     end
-
   end
 
+  def customer_params
+    params.required(:customer).permit(:name, :document, :active)
+  end
 end
--- onboarding/config/routes.rb
+++ onboarding/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/customer/create' => 'customer#create', as: :create_customer
-  get '/customer/find_all' => 'customer#find_all', as: :find_all
+  #post '/customer/create' => 'customer#create', as: :create_customer
+  #get '/customer/find_all' => 'customer#find_all', as: :find_all
+  resources :customers do
+    get 'find_all'
+  end
 end
